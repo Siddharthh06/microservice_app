6a2faff387fc14417f065d48bc68d532da54b44b
Siddharth Gupta <siddharthdgupta@gmail.com>
2023-10-06 12:18:08 +0530

    product-service added

---
+++ product-service/src/main/java/com/microserviceproject/productservice/controller/ProductController.java
@@ -0,0 +1,25 @@
+package com.microserviceproject.productservice.controller;
+
+import com.microserviceproject.productservice.dto.ProductRequest;
+import com.microserviceproject.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest) {
+        productService.createProduct(productRequest);
+    }
+
+
+
+}
