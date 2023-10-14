bf55047452f7c197ce75052ed95454abd02453ef
Siddharth Gupta <siddharthdgupta@gmail.com>
2023-10-14 23:20:53 +0530

    product service tested with req

--- product-service/src/main/java/com/microserviceproject/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/microserviceproject/productservice/ProductServiceApplication.java
@@ -2,11 +2,13 @@ package com.microserviceproject.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProductServiceApplication {
+public class ProductServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 
--- product-service/src/main/java/com/microserviceproject/productservice/repository/ProductRepository.java
+++ product-service/src/main/java/com/microserviceproject/productservice/repository/ProductRepository.java
@@ -4,4 +4,6 @@ import com.microserviceproject.productservice.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ProductRepository extends MongoRepository<Product, String> {
+
 }
+
