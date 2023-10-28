79ea9b5a3f92754176da96ba9471f911560ec50e
Siddharth Gupta <siddharthdgupta@gmail.com>
2023-10-29 01:01:06 +0530

    inventory service code update

--- inventory-service/src/main/java/com/microserviceproject/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/microserviceproject/inventoryservice/repository/InventoryRepository.java
@@ -4,7 +4,11 @@ import com.microserviceproject.inventoryservice.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 
 @Repository
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    Optional<Inventory> findBySkuCode();
 }
