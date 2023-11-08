1f68c2e00be978c667dd86695b994183b6d2dbb8
Siddharth Gupta <siddharthdgupta@gmail.com>
2023-11-08 16:20:13 +0530

    inter service communication

--- OnlineShoppingApp/inventory-service/src/main/java/com/microserviceproject/inventoryservice/controller/InventoryController.java
+++ OnlineShoppingApp/inventory-service/src/main/java/com/microserviceproject/inventoryservice/controller/InventoryController.java
@@ -1,10 +1,13 @@
 package com.microserviceproject.inventoryservice.controller;
 
+import com.microserviceproject.inventoryservice.dto.InventoryResponse;
 import com.microserviceproject.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
@@ -14,16 +17,22 @@ public class InventoryController {
 
     private final InventoryService inventoryService;
 
-//    @GetMapping
-//    @ResponseStatus(HttpStatus.OK)
-//    public String getServerStatus() {
-//        return "server is running";
-//    }
-
-    @GetMapping("/{sku-code}")
+    // this mapping is used when only 1 skucode is passed as the request parameter
+    // the link is of the form:
+        // http://localhost:8080/inventory-service/api/inventory/iphone_13
+    /* @GetMapping("/{sku-code}")
     @ResponseStatus(HttpStatus.OK)
     public boolean isInStock(@PathVariable("sku-code") String skuCode) {
         log.info("Received inventory check request for skuCode: {}", skuCode);
+        return inventoryService.isInStock(skuCode);
+    }
+    */
+
+    // to pass multiple skucode as the request parameter of the form:
+        // http://localhost:8080/inventory-service/api/inventory/skuCode=iphone_13&skuCode=iphone_13_red
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
         return inventoryService.isInStock(skuCode);
     }
 }
--- OnlineShoppingApp/inventory-service/src/main/java/com/microserviceproject/inventoryservice/repository/InventoryRepository.java
+++ OnlineShoppingApp/inventory-service/src/main/java/com/microserviceproject/inventoryservice/repository/InventoryRepository.java
@@ -5,10 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.swing.text.html.Option;
+import java.util.List;
 import java.util.Optional;
 
 
 @Repository
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
-    Optional<Inventory> findBySkuCode(String skuCode);
+//    Optional<Inventory> findBySkuCode(String skuCode);
+
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
 }
